c36d8e93efeb561fb4e5577a3de37f03d65e0ee1
Santiago Rosas <87668523+RosasSantiago@users.noreply.github.com>
2021-07-26 23:58:53 -0300

    Update Json-Santi.py

--- Json-Santi.py
+++ Json-Santi.py
@@ -1,7 +1,16 @@
+# Imports
 import json
+
+with open("cristian.json") as cristian:
+    cristian_dict = json.load(cristian)
+
+with open("cristian.json","w") as cristian:
+    json.dump(person_dict, cristian)
 
 with open("Articulos.json") as client:
     client_dict = json.loads(client.read())
+    
+    
 print(client_dict)
 print(client_dict["name"])
 print(client_dict["intereses"])
@@ -13,11 +22,5 @@ person_dict = {"name": "Cristian"
                 "last name": "Rosas"
                 "sobrenombre": "Chino"
                 }
-
-with open("cristian.json","w") as cristian:
-    json.dump(person_dict, cristian)
-
-with open("cristian.json") as cristian:
-    cristian_dict = json.load(cristian)
 
 print(cristian_dict)
